4f4766f4451c510c6a3982f7b5602be04fbad224
Michal Marciniec <michal.marciniec@codete.co>
2016-05-06 13:07:17 +0200

    # Swagger integration

--- src/main/java/com/codete/codeball/Application.java
+++ src/main/java/com/codete/codeball/Application.java
@@ -2,8 +2,10 @@ package com.codete.codeball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.codete.codeball")
 public class Application {
 
     public static void main(String[] args) {
