c833e736181dc20214c1529b765b4f6c174af034
Michal Marciniec <michal.marciniec@codete.co>
2016-05-09 17:42:35 +0200

    #9 - enabled CORS for localhost:3000 (fix)

--- src/main/java/com/codete/codeball/config/SecurityConfiguration.java
+++ src/main/java/com/codete/codeball/config/SecurityConfiguration.java
@@ -1,10 +1,11 @@
-package com.codete.codeball;
+package com.codete.codeball.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -21,19 +22,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable();
 
         http
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+
+        http
                 .antMatcher("/**").authorizeRequests()
                 .antMatchers("/**", "/login**", "/webjars/**").permitAll()
                 .anyRequest().authenticated();
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*").allowedOrigins("http://localhost:3000");
-            }
-        };
     }
 
 }
