2f306b3658e33666eda361088625991c2436062f
Michal Marciniec <michal.marciniec@codete.co>
2016-05-20 08:56:12 +0200

    #9 - CORS configuration fix for localhost:3000

---
+++ src/main/java/com/codete/codeball/config/CORSConfig.java
@@ -0,0 +1,19 @@
+package com.codete.codeball.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+
+}
--- src/main/java/com/codete/codeball/config/SecurityConfiguration.java
+++ src/main/java/com/codete/codeball/config/SecurityConfiguration.java
@@ -1,14 +1,9 @@
 package com.codete.codeball.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableOAuth2Sso
@@ -20,9 +15,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable();
-
-        http
-                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
 
         http
                 .antMatcher("/**").authorizeRequests()
