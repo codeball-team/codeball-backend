f68272192e19d26c800c53aaf27bcdcde21dc93f
Michal Marciniec <michal.marciniec@codete.co>
2016-05-20 09:13:56 +0200

    #9 - CORS configuration fix for localhost:8080

--- src/main/java/com/codete/codeball/config/CORSConfig.java
+++ src/main/java/com/codete/codeball/config/CORSConfig.java
@@ -2,17 +2,16 @@ package com.codete.codeball.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class CORSConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 
