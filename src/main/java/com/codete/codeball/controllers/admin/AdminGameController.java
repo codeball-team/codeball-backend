5858a062be5673d3ceb9be7f117f814603340787
Michal Marciniec <michal.marciniec@codete.co>
2016-06-15 20:20:17 +0200

    #28 Admin CRUD controller for games

--- src/main/java/com/codete/codeball/controllers/admin/AdminGameController.java
+++ src/main/java/com/codete/codeball/controllers/admin/AdminGameController.java
@@ -1,42 +1,42 @@
 package com.codete.codeball.controllers.admin;
 
-import com.codete.codeball.model.User;
-import com.codete.codeball.repositories.UserRepository;
+import com.codete.codeball.model.Game;
+import com.codete.codeball.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/admin/user")
-public class AdminUserController {
+@RequestMapping(value = "/api/admin/game")
+public class AdminGameController {
 
     @Autowired
-    private UserRepository userRepository;
+    private GameRepository gameRepository;
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public Iterable<User> getUsers() {
-        return userRepository.findAll();
+    public Iterable<Game> getGames() {
+        return gameRepository.findAll();
     }
 
     @RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public User getUser(@PathVariable long id) {
-        return userRepository.findOne(id);
+    public Game getGameById(@PathVariable long id) {
+        return gameRepository.findOne(id);
     }
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public Game createGame(@RequestBody Game game) {
+        return gameRepository.save(game);
     }
 
     @RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
-    public User updateUser(@PathVariable long id, @RequestBody User user) {
-        user.setId(id);
-        return userRepository.save(user);
+    public Game updateGame(@PathVariable long id, @RequestBody Game game) {
+        game.setId(id);
+        return gameRepository.save(game);
     }
 
     @RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
-    public void deleteUser(@PathVariable long id) {
-        userRepository.delete(id);
+    public void deleteGame(@PathVariable long id) {
+        gameRepository.delete(id);
     }
 
 }
