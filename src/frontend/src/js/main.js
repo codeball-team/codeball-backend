6982d905377b89251f9a90422f852d97884e919f
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2016-07-23 03:32:50 +0200

    Adding fastclick

--- src/frontend/src/js/main.js
+++ src/frontend/src/js/main.js
@@ -2,6 +2,7 @@ import 'node-normalize-scss/_normalize.scss';
 import 'styles/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FastClick from 'fastclick';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { Router, browserHistory } from 'react-router';
@@ -9,10 +10,11 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import { BodyBackground } from 'components/ui';
 import { BACKGROUND_IMAGES } from 'constants';
 import routes from './routes';
-
 const store = configureStore(undefined, browserHistory);
 const rootElement = document.getElementById('app');
 const history = syncHistoryWithStore(browserHistory, store);
+
+FastClick.attach(document.body);
 
 ReactDOM.render(
   <Provider store={store}>
