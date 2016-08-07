3d2c1d80e1a0aa657cbaf898eea6cf0918eaba54
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2016-08-07 22:38:52 +0200

    Restructuring form components

--- src/frontend/src/js/components/ui/index.js
+++ src/frontend/src/js/components/ui/index.js
@@ -12,20 +12,21 @@ export * from './buttons';
 export BodyBackground from './BodyBackground/BodyBackground';
 export Button from './Button/Button';
 export ButtonsPanel from './ButtonsPanel/ButtonsPanel';
-export EditableText from './EditableText/EditableText';
 export Errors from './Errors/Errors';
-export Form from './Form/Form';
+export Form, {
+  EditableText,
+  InputWrapper,
+  NumberPicker,
+  RangePicker,
+  ValuePicker
+} from './Form';
 export Icon from './Icon/Icon';
 export IconButton from './IconButton/IconButton';
-export InputWrapper from './InputWrapper/InputWrapper';
 export List from './List/List';
 export ListItem from './ListItem/ListItem';
 export LoadableContent from './LoadableContent/LoadableContent';
 export Navigation from './Navigation/Navigation';
 export NotLoaded from './NotLoaded/NotLoaded';
-export NumberPicker from './NumberPicker/NumberPicker';
 export Page from './Page/Page';
-export RangePicker from './RangePicker/RangePicker';
 export Section from './Section/Section';
 export Spinner from './Spinner/Spinner';
-export ValuePicker from './ValuePicker/ValuePicker';
