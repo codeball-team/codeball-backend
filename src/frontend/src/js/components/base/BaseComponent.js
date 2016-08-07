9e26210cd8e7507db6a5744bf5df13c325cbe576
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2016-08-07 23:02:33 +0200

    Pure render in base component

--- src/frontend/src/js/components/base/BaseComponent.js
+++ src/frontend/src/js/components/base/BaseComponent.js
@@ -1,5 +1,12 @@
 import ConditionalRender from './ConditionalRender';
+import shallowCompare from 'react-addons-shallow-compare';
 
 export default function BaseComponent(ComponentClass) {
-  return ConditionalRender(ComponentClass);
+  const BaseComponentClass = ConditionalRender(ComponentClass);
+
+  BaseComponentClass.prototype.shouldComponentUpdate = function(nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState);
+  };
+
+  return BaseComponentClass;
 }
