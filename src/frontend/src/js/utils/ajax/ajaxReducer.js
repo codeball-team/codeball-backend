6b0cb1ef948308777498508fe0acce491c5f21e4
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2016-09-20 00:39:19 +0200

    Re-adding file lost due to a merge conflict

--- src/frontend/src/js/utils/ajax/ajaxReducer.js
+++ src/frontend/src/js/utils/ajax/ajaxReducer.js
@@ -1,9 +1,11 @@
-import { _, reducer } from 'utils';
+import { reducer, safeGet } from 'utils';
+import md5 from 'js-md5';
 
 const ajaxReducerInitialState = {
   isLoading: false,
   hasLoaded: false,
-  lastUpdate: undefined
+  lastUpdateTimestamp: undefined,
+  lastUpdateHash: md5(JSON.stringify(''))
 };
 
 export default function ajaxReducer(initialState, ajaxActions, handlers) {
@@ -21,39 +23,66 @@ export default function ajaxReducer(initialState, ajaxActions, handlers) {
   const originalReducer = reducer(reducerInitialState, handlers);
 
   return (state = reducerInitialState, action) => {
+    const { lastUpdateHash } = state;
     const { type } = action;
-    const newState = {
+    const ajaxHandler = ajaxHandlers[type];
+
+    if(ajaxHandler) {
+      const newState = ajaxHandler(state, action);
+      const hasHashChanged = lastUpdateHash !== newState.lastUpdateHash;
+
+      if(hasHashChanged) {
+        return {
+          ...newState,
+          ...originalReducer(newState, action)
+        };
+      }
+
+      return newState;
+    }
+
+    return {
       ...state,
       ...originalReducer(state, action)
     };
-
-    if(_(ajaxHandlers).has(type)) {
-      const handler = ajaxHandlers[type];
-      const enhancedNewState = handler(newState, action);
-      return enhancedNewState;
-    }
-
-    return newState;
   };
 }
 
 function onAjaxStart(state, action) {
-  return onUpdate({ ...state, isLoading: true }, action);
+  return onUpdate({
+    ...state,
+    isLoading: true
+  }, action);
 }
 
 function onAjaxSuccess(state, action) {
-  return { ...onAjaxEnd(state, action), hasLoaded: true };
+  return {
+    ...onAjaxEnd(state, action),
+    hasLoaded: true
+  };
 }
 
 function onAjaxFail(state, action) {
-  return { ...onAjaxEnd(state, action), hasLoaded: false };
+  return {
+    ...onAjaxEnd(state, action),
+    hasLoaded: false
+  };
 }
 
 function onAjaxEnd(state, action) {
-  return onUpdate({ ...state, isLoading: false }, action);
+  const responseBody = safeGet(action, ['response', 'body'], '');
+  const responseHash = md5(JSON.stringify(responseBody));
+  return onUpdate({
+    ...state,
+    isLoading: false,
+    lastUpdateHash: responseHash
+  }, action);
 }
 
 function onUpdate(state, action) {
-  const { time: lastUpdate } = action;
-  return { ...state, lastUpdate };
+  const { timestamp: lastUpdateTimestamp } = action;
+  return {
+    ...state,
+    lastUpdateTimestamp
+  };
 }
